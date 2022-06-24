00b8d31716e50a8e369600bfc056cd035471fd54
George.Pomaskin <pomaskin@utip.ru>
2022-06-24 19:06:10 +0400

    real SIMD is used

--- build-wasm.bat
+++ build-wasm.bat
@@ -1,6 +1,7 @@
 call emsdk_env
 em++ ^
 -I include/Eigen/ ^
+ -msimd128 ^
 -s WASM=1 -s MODULARIZE=1 -O3 ^
 -s "EXPORTED_FUNCTIONS=['_malloc', '_free', '_setupSimulation', '_tick']" ^
 src/cpp/main.cpp ^
